42dc8be323119149412c3349446ec7e701c3daf1
Ben McIntyre <Ben.McIntyre@simpson-associates.co.uk>
2021-03-28 15:02:57 +0100

    Disable aide

--- ubuntu.sh
+++ ubuntu.sh
@@ -144,7 +144,7 @@ function main {
   f_cron
   f_ctrlaltdel
   f_auditd
-  f_aide
+#  f_aide
   f_rhosts
   f_users
   f_lockroot
@@ -154,8 +154,8 @@ function main {
   f_umask
   f_path
   f_aa_enforce
-  f_aide_post
-  f_aide_timer
+#  f_aide_post
+#  f_aide_timer
   f_aptget_noexec
   f_aptget_clean
   f_systemddelta
